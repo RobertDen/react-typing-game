34128202685c7c80170d622473cee15e47192708
Robert Denning <robertd5@umbc.edu>
2025-01-10 12:56:00 -0500

    Fix: resolve MdRefresh JSX error in RestartButton

--- speed-typing/src/App.tsx
+++ speed-typing/src/App.tsx
@@ -1,13 +1,27 @@
 import { faker } from "@faker-js/faker";
+import RestartButton from "./components/RestartButton";
 
 const words = faker.word.words(10);
 
 const App = () => {
-  return<GeneratedWords words={words} />;
+  return (
+    <>
+      <CountdownTimer timeLeft={30} />
+      <GeneratedWords words={words} />
+      <RestartButton
+        className={"mx-auto mt-10 text-slate-500"}
+        onRestart={() => null}  
+        />
+    </>
+  );
 };
 
 const GeneratedWords = ({ words }: { words: string }) => {
   return <div className="text-4xl text-center text-slate-500">{words}</div>;
+};
+
+const CountdownTimer = ({ timeLeft }: { timeLeft: number }) => {
+  return <h2 className="text-primary-400 font-medium">Time: {timeLeft}</h2>;
 };
 
 export default App;
