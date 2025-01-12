7367e17ea73f706c21c5a287999279345531cde5
Robert Denning <robertd5@umbc.edu>
2025-01-12 18:38:00 -0500

    Added results component and fixed syntax error in RestartButton component

--- speed-typing/src/App.tsx
+++ speed-typing/src/App.tsx
@@ -1,5 +1,6 @@
 import { faker } from "@faker-js/faker";
 import RestartButton from "./components/RestartButton";
+import Results from "./components/Results";
 
 const words = faker.word.words(10);
 
@@ -11,7 +12,13 @@ const App = () => {
       <RestartButton
         className={"mx-auto mt-10 text-slate-500"}
         onRestart={() => null}  
-        />
+      />
+      <Results
+        className="mt-10"
+        errors={10}
+        accuracyPercentage={100}
+        total={200}
+      />
     </>
   );
 };
