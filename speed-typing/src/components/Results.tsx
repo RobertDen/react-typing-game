20f24c96b506019f7ba14f025b8cf1ec8d84a949
Robert Denning <robertd5@umbc.edu>
2025-01-13 12:45:00 -0500

    Added helper to format percentages from Results

--- speed-typing/src/components/Results.tsx
+++ speed-typing/src/components/Results.tsx
@@ -1,3 +1,5 @@
+import {formatPercentage} from "../utils/helpers";
+
 const Results = ({
     errors,
     accuracyPercentage,
@@ -14,7 +16,7 @@ const Results = ({
             className={`flex flex-col items-center text-primary-400 space-y-3 ${className}`}
             >
                 <li className="text-xl font-semibold">Results</li>
-                <li>Accuracy: {accuracyPercentage}</li>
+                <li>Accuracy: {formatPercentage(accuracyPercentage)}</li>
                 <li className="text-red-500">Errors : {errors}</li>
                 <li>Typed: {total}</li>
             </ul>
